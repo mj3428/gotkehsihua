d169e5125aafa43438c0f7eaed5b9040ba6129eb
mj3428 <38501129+mj3428@users.noreply.github.com>
2018-09-05 11:45:30 +0800

    Update ARMR_model.py

--- ARMR_model.py
+++ ARMR_model.py
@@ -1,3 +1,5 @@
+##文艺数学君博客：https://blog.csdn.net/wmn7q/article/details/64129929
 '''
 详见:https://blog.csdn.net/dingming001/article/details/73554949
 时间序列分析(1):https://blog.csdn.net/wmn7q/article/details/64129929
+时间序列分析(2):https://blog.csdn.net/WMN7Q/article/details/66472665
