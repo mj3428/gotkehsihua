878a13704d368b540075d0b66bc62ca83f10c27b
mj3428 <38501129+mj3428@users.noreply.github.com>
2018-09-05 13:59:11 +0800

    Update ARMR_model.py

--- ARMR_model.py
+++ ARMR_model.py
@@ -3,3 +3,4 @@
 详见:https://blog.csdn.net/dingming001/article/details/73554949
 时间序列分析(1):https://blog.csdn.net/wmn7q/article/details/64129929
 时间序列分析(2):https://blog.csdn.net/WMN7Q/article/details/66472665
+时间序列分析(3)--自相关系数和偏自相关系数:https://blog.csdn.net/WMN7Q/article/details/70174300
