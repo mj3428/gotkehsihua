94501181701ae5d975d62ae8ba75c1b451033e0c
mj3428 <38501129+mj3428@users.noreply.github.com>
2018-09-05 14:02:04 +0800

    Update ARMR_model.py

--- ARMR_model.py
+++ ARMR_model.py
@@ -1,6 +1,7 @@
 ##文艺数学君博客：https://blog.csdn.net/wmn7q/article/details/64129929
 '''
 详见:https://blog.csdn.net/dingming001/article/details/73554949
+回归分析(1)--协方差与相关系数:https://blog.csdn.net/wmn7q/article/details/52594607
 时间序列分析(1):https://blog.csdn.net/wmn7q/article/details/64129929
 时间序列分析(2):https://blog.csdn.net/WMN7Q/article/details/66472665
 时间序列分析(3)--自相关系数和偏自相关系数:https://blog.csdn.net/WMN7Q/article/details/70174300
