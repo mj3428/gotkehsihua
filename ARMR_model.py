9573a77e5abc276c89fc862dea75809672743156
mj3428 <38501129+mj3428@users.noreply.github.com>
2018-09-07 15:57:09 +0800

    Update ARMR_model.py

--- ARMR_model.py
+++ ARMR_model.py
@@ -6,6 +6,8 @@
 时间序列分析(2):https://blog.csdn.net/WMN7Q/article/details/66472665
 时间序列分析(3)--自相关系数和偏自相关系数:https://blog.csdn.net/WMN7Q/article/details/70174300
 时间序列分析(4)--AR模型,MA模型,ARMA模型介绍:https://blog.csdn.net/WMN7Q/article/details/70301215
+时间序列分析(5)--非平稳时间序列模型与差分:https://blog.csdn.net/WMN7Q/article/details/70477985
+时间序列分析(6)--季节差分:https://blog.csdn.net/WMN7Q/article/details/72354887
 
 
 一些markdown的快捷键
