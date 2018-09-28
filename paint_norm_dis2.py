a94e84313c5633022e9b8f1824f8438d2606a5f5
mj3428 <38501129+mj3428@users.noreply.github.com>
2018-09-28 19:29:44 +0800

    Update paint_norm_dis2.py

--- paint_norm_dis2.py
+++ paint_norm_dis2.py
@@ -10,6 +10,7 @@
 """
 import matplotlib as mpl
 #mpl.use('Qt5Agg')
+#print(mpl.matplotlib_fname())
 import matplotlib.pyplot as plt
 import pandas as pd
 from pandas import DataFrame
@@ -45,11 +46,13 @@ def normfun(x,mu,sigma):
 mu = np.mean([df.Ua,df.Ub,df.Uc],axis=1)
 #mu = np.mean(df.Ua)
 sigma = np.std([df.Ua,df.Ub,df.Uc],axis=1)
-print (mu,sigma)
+print (mu[1],sigma[1])
 
 #设置字体
-mpl.rcParams['font.sans-serif'] = ['Microsoft YaHei'] # 指定默认字体
-mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+mpl.rcParams['font.family']='sans-serif'#解决负号'-'显示为方块的问题
+mpl.rcParams['axes.unicode_minus'] = False
+
 
 #画布大小 长 宽 dpi分辨率 facecolor背景颜色
 fig = plt.figure(figsize=[12.0,4.0],dpi=144,facecolor='gainsboro')
@@ -60,16 +63,16 @@ fig = plt.figure(figsize=[12.0,4.0],dpi=144,facecolor='gainsboro')
 plt.subplot(131)
 n_a, bins_a, patches_c = plt.hist(df.Ua,bins=13,alpha=0.6,\
                                   rwidth=0.9,color='orange',\
-                                  label='Probability',density=False)
+                                  label='Probability',density=True)
 length_a = plt.xlim()
 width_a = (length_a[1]-length_a[0])/(len(bins_a)-1)
 print (n_a,length_a,width_a,np.sum(n_a)) #n代表直方图的柱子的y值
 #显示数字标签 ha代表对齐方式
 for a,b in zip(bins_a,n_a):
-    plt.text(a,b + 0.0025,'%.1f%%'%(b/np.sum(n_a)*100),ha='left', va= 'bottom',fontsize=6)
+    plt.text(a,b + 0.0025,'%.1f%%'%(b*width_a*100),ha='left', va= 'bottom',fontsize=6)
 #拟合Ua
 y_a = stats.norm.pdf(bins_a, mu[0], sigma[0])
-norm_a = plt.plot(bins_a, y_a*1000, color='gold',linestyle='--',label='norm')
+norm_a = plt.plot(bins_a, y_a, color='gold',linestyle='--',label='norm')
 #画参考线
 plt.vlines(235,0,0.17,colors=(0.953,0.641,0.406),linestyles='-.',label='Upper 235V')
 plt.legend(fontsize=6,loc='upper left')
@@ -80,15 +83,15 @@ plt.ylabel('Pdf',fontsize=9)
 plt.subplot(132)
 n_b, bins_b, patches_b = plt.hist(df.Ub,bins=13,alpha=0.6,\
                                   rwidth=0.9,color='mediumseagreen', \
-                                  label='Probability',density=False) #这里norm用来拟合正态分布
+                                  label='Probability',density=True) #这里norm用来拟合正态分布
 length_b = plt.xlim()
 width_b = (length_b[1]-length_b[0])/(len(bins_b)-1)
 print (n_b,length_b,width_b) #n代表直方图的柱子的y值
 for a,b in zip(bins_b,n_b):
-    plt.text(a,b + 0.0025,'%.1f%%'%(b/np.sum(n_b)*100),ha='left', va= 'bottom',fontsize=6)
+    plt.text(a,b + 0.0025,'%.1f%%'%(b*width_b*100),ha='left', va= 'bottom',fontsize=6)
 #拟合Ub
-y_b = stats.norm.pdf(bins_b, mu[0], sigma[0])
-norm_b = plt.plot(bins_b, y_b*1000, 'g--',label='norm')
+y_b = stats.norm.pdf(bins_b, mu[1], sigma[1])
+norm_b = plt.plot(bins_b, y_b, 'g--',label='norm')
 #画参考线
 plt.vlines(235,0,0.165,colors=(0.395,0.816,0.766),linestyles='-.',label='Upper 235V')
 plt.legend(fontsize=6,loc='upper left')
@@ -99,15 +102,15 @@ plt.ylabel('Pdf',fontsize=9)
 plt.subplot(133)
 n_c, bins_c, patches_c = plt.hist(df.Uc,bins=13,alpha=0.6,\
                                   rwidth=0.9,color='orangered', \
-                                  label='Probability',density=False) #这里norm用来拟合正态分布
+                                  label='Probability',density=True) #这里norm用来拟合正态分布
 length_c = plt.xlim()
 width_c = (length_c[1]-length_c[0])/(len(bins_c)-1)
 print (n_c,length_c,width_c) #n代表直方图的柱子的y值
-for a,b in zip(bins_a,n_a):
-    plt.text(a,b + 0.001,'%.1f%%'%(b/np.sum(n_c)*100),ha='left', va= 'bottom',fontsize=6)
+for a,b in zip(bins_c,n_c):
+    plt.text(a,b + 0.0025,'%.1f%%'%(b*width_c*100),ha='left', va= 'bottom',fontsize=6)
 #拟合Uc
-y_c = stats.norm.pdf(bins_c, mu[0], sigma[0])
-norm_c = plt.plot(bins_c, y_c*1000, 'r--',label='norm')
+y_c = stats.norm.pdf(bins_c, mu[2], sigma[2])
+norm_c = plt.plot(bins_c, y_c, 'r--',label='norm')
 #画参考线
 plt.vlines(235,0,0.165,colors=(0.957,0.625,0.559),linestyles='-.',label='Upper 235V')
 plt.legend(fontsize=6,loc='upper left')
@@ -131,4 +134,5 @@ plt.suptitle('A、B、C三相电压分布图')
 #w_pad用于设置绘图区间水平距离的大小
 #h_pad用于设置绘图区间垂直距离的大小
 fig.tight_layout(pad=2.3)
-plt.show(block=True)
+#plt.show(block=True)
+plt.show()
