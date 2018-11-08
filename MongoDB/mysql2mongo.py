d3913430b52add04d08a4fe781056de983562a69
mj3428 <38501129+mj3428@users.noreply.github.com>
2018-11-08 14:40:40 +0800

    Update mysql2mongo.py

--- MongoDB/mysql2mongo.py
+++ MongoDB/mysql2mongo.py
@@ -10,22 +10,19 @@
 '''
 import mysql.connector
 import pymongo
-
-#mongo配置
-MONGO_URL = 'localhost'
-MONGO_DB = 'zjnad'
-MONGO_COLLECTION = '2018sep'
-client = pymongo.MongoClient(MONGO_URL)
-db = client[MONGO_DB]
+import json
+import pandas as pd
+import funcy as fy
+from pandas import DataFrame
 
 
-table_names = ['2018sep','2018Oct']
+table_names = ['2018sep']
 #配置数据库的基本信息
 config = {'host': '127.0.0.1',
           'user': 'root',
-          'password': 'mj3428',
+          'password': '*****',
           'port': 3306,
-          'database': 'zjnad',
+          'database': '****',
           'charset': 'gbk'
           }
 con = mysql.connector.connect(**config)
@@ -33,31 +30,108 @@ cursor = con.cursor()
 
 sql = 'select * from %s'%(table_names[0])
 cursor.execute(sql)
-rows = cursor.fetchall()
 column_names = [d[0] for d in cursor.description]
+rows = cursor.fetchall()
+#用pymysql可以这样用，如果是engine可以用pandas直接读取
+row_frame = DataFrame(list(rows),columns=column_names)
+#print(rows) 显示为数列，里面包含元组
+cursor.close()
+print(column_names) #表头
 
-#定义一个继承自dict的类
-class Row(dict):
-    """A dict that allows for object-like property access syntax."""
-    def __getattr__(self, name):
-        try:
-            return self[name]
-        except KeyError:
-            raise AttributeError(name)
+#pymongo
+class MongodbConfig(object):
+    #配置数据库
+    host = 'localhost'
+    port = 27017
+    id = 'id'
+    user = 'user'
+    uid = 'uid'
+    events = 'events'
+    place = 'place'
+    content = 'content'
+    rank = 'rank'
+    freq = 'freq'
+    state = 'state'
+    time = 'time'
 
-#定义导入mongo
-def save_to_mongo(result):
-    """
-    保存至MongoDB
-    :param result: 结果
-    """
-    try:
-        if db[MONGO_COLLECTION].insert_many(result):
-            print('=success insert into mongodb')
-    except Exception:
-        print('fail')
+    def __init__(self, database, collection, host=None, port=None):
+        self.host = host if host else self.host
+        self.port = port if port else self.port
+        self.database = database
+        self.collection = collection
 
-#将数据转换成json格式：
-items = [Row(zip(column_names, row)) for row in rows]
-for item in items:
-    #########没写完，有问题########
+    def _set_collection(self):
+        """设置数据库"""
+        client = pymongo.MongoClient(host=self.host, port=self.port)
+        db = client[self.database]
+        Collection = db[self.collection]
+
+        return Collection
+
+    def __load_dataframe(self):
+        """读取CSV"""
+        df = row_frame
+        j = df.to_json()
+        data = json.loads(j)
+        return data
+
+    def _combine_and_insert(self, data):
+        """整合并插入数据"""
+        # 构造 index 列表
+        name_list = [self.id, self.user, self.uid, self.events, self.place,
+                     self.content, self.rank, self.freq, self.state, self.time]
+        # 删除 None
+
+        for i in range(len(name_list)):
+            if None in name_list:
+                name_list.remove(None)
+
+        def process_data(n):
+            # 返回单个数据的字典，key为index，若无index则返回 None
+            single_data = {index.lower(): data[index].get(str(n))
+                           for index in name_list}
+
+            return single_data
+
+        lenth = len(data[self.id])  # 总长度
+        coll = self._set_collection()
+
+        # 插入数据
+
+        for i in range(lenth):
+            bar = process_data(i)
+            coll.insert_one(bar)
+            print('Inserting ' + str(i) + ', Total: ' + str(lenth))
+
+    def __get_dups_id(self, data):
+        """获得重复数据的id"""
+        data['dups'].pop(0)
+
+        return data['dups']
+
+    def _drop_duplicates(self):
+        """删除重复数据"""
+        coll = self._set_collection()
+        c = coll.aggregate([{"$group":
+                                 {"_id": {'date': '$date'},
+                                  "count": {'$sum': 1},
+                                  "dups": {'$addToSet': '$_id'}}},
+                            {'$match': {'count': {"$gt": 1}}}
+                            ]
+                           )
+        data = [i for i in c]
+        duplicates = fy.walk(self.__get_dups_id, data)
+        dups_id_list = fy.cat(duplicates)
+
+        for i in dups_id_list:
+            coll.delete_one({'_id': i})
+        # print("OK, duplicates droped! Done!")
+
+    def data_to_db(self):
+        """数据导入数据库"""
+        data = self.__load_dataframe()
+        self._combine_and_insert(data)
+        self._drop_duplicates()
+
+MGDB = MongodbConfig(database='zjnad',collection='2018sep')
+MGDB.data_to_db()
